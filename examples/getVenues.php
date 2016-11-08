4834f3b4a1bb78d17bfc4e667d949fb90d294b2b
Sean McNamara <sean.mcnamara@kobas.co.uk>
2016-11-08 10:43:40 +0000

    Add example for get Venues.

---
+++ examples/getVenues.php
@@ -0,0 +1,9 @@
+<?php
+$companyId   = 0; // Company ID
+$staffId     = 0; //Staff ID
+$staffSecret = ''; // Staff API Key
+
+$signer = new \Kobas\Auth\Signer($companyId, $staffId, $staffSecret);
+$client = new \Kobas\Client($signer);
+
+$venues = $client->get('data/venue');
