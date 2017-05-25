8821904b7c7bf79d5d301191639a1ad7505c674f
aleeeftw <alexandru.aleee@gmail.com>
2017-05-25 14:17:34 +0300

    Updated Wireless Social example.

--- examples/wirelessSocial.php
+++ examples/wirelessSocial.php
@@ -9,6 +9,6 @@ $secret     = '...'; // API Key
 $signer = (new \Kobas\Auth\Signer($companyId, $identifier, $secret));
 $client = (new \Kobas\Client($signer));
 
-$providedExample = json_decode('[{"id": 1665454,"occurredAt": "2017-02-01 10:00:00","isFirstSightingOfUser": true,"user": {"id": 55566,"emailAddress": "abc@def.com","firstName": "john","lastName": "farrimond","dateOfBirth": "1985-01-01","gender": "M","location": "leyland, lancashire","postcode": "PR25 3GR","mobilePhoneNumber": "07712345679","firstSeenAt": "2017-02-01 10:00:00","lastSeenAt": "2017-02-01 10:00:00","numLogins": 1,"dataSource": "facebook","dataSourceId": "14545654446"},"venue": {"id": 5554,"name": "bob\'s cafe","customerRef": "1"}}]', true);
+$providedExample = json_decode('{"customer":{"id": 1665454,"occurredAt": "2017-02-01 10:00:00","isFirstSightingOfUser": true,"user": {"id": 55566,"emailAddress": "abc@def.com","firstName": "john","lastName": "farrimond","dateOfBirth": "1985-01-01","gender": "M","location": "leyland, lancashire","postcode": "PR25 3GR","mobilePhoneNumber": "07712345679","firstSeenAt": "2017-02-01 10:00:00","lastSeenAt": "2017-02-01 10:00:00","numLogins": 1,"dataSource": "facebook","dataSourceId": "14545654446"},"venue": {"id": 5554,"name": "bob\'s cafe","customerRef": "1"}}}', true);
 
-$request = $client->post('wifi-login/wireless-social', $providedExample);
+$request = $client->post('integrations/wifi-login/wireless-social', $providedExample);
